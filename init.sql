ff8ef4d45a067479e6a9ba2f8bb84b67c03ae5f4
George-Zhang5246 <70183314+George-Zhang5246@users.noreply.github.com>
2024-04-25 15:58:18 -0500

    Update init.sql
    Finished creating all tables

--- init.sql
+++ init.sql
@@ -1,6 +1,30 @@
 use bitcoindb;
 
 CREATE TABLE client (clientID int, name varchar(50), phoneNumber int, cellPhoneNumber int, email varchar(50),
-level varchar(20), state varchar(20), zipcode int, city varchar(50), stAddress varchar(50), primary key (clientID);
+  isGold bit, stateName varchar(20), zipcode int, city varchar(50), stAddress varchar(50), monthlyTrades int, 
+  bitcoin real, fiat real, clientID int, password varchar(50),
+  primary key (clientID));
 
-CREATE TABLE account (monthlyTrades int, bitcoin real, accountID int);
+--CREATE TABLE account (monthlyTrades int, bitcoin real, fiat real, clientID int, password varchar(50), primary key (clientID));
+
+CREATE TABLE trader (traderID int, name varchar(50), 
+  primary key (traderID))
+
+CREATE TABLE transaction (transactionID int, clientID int, traderID int, transactionDate date,
+  primary key (transactionID),
+  foreign key (clientID) references client(clientID),
+  foreign key (traderID) references trader(traderID))
+
+CREATE TABLE bitcoinTransaction (transactionID int, fiatCommission bit,
+  commission real, fiat real, bitcoin real,
+  primary key (transactionID),
+  foreign key (transactionID) references transaction(transactionID))  
+
+CREATE TABLE paymentTransaction (transactionID int, fiatAmount int,
+  primary key (transactionID),
+  foreign key (transactionID) references transaction(transactionID)) 
+
+CREATE TABLE cancel (transactionID int, cancelDate date, traderID int,
+  primary key (transactionID),
+  foreign key (transactionID) references transaction(transactionID),
+  foreign key (traderID) references trader(traderID)))
