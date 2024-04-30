46a54f76b7816da1a4045b0465e4d2da53226eb0
George-Zhang5246 <70183314+George-Zhang5246@users.noreply.github.com>
2024-04-30 14:50:08 -0500

    Update urls.py
    Added names

--- website/urls.py
+++ website/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import views
 
 urlpatterns = [
-    path("login/", views.login),
-    path("client-sign-up/", views.signup),
-    path("client/", views.client),
-    path("trader/", views.trader),
+    path("login/", views.login, name = "login"),
+    path("client-sign-up/", views.signup, name = "signup"),
+    path("client/", views.client, name = "client"),
+    path("trader/", views.trader, name = "trader"),
 ]
