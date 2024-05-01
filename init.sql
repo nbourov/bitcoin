8a56733a1f10513ec919f8060676d2b8d15200c7
nbourov <nbourov@gmail.com>
2024-04-30 20:10:38 -0500

    Update init.sql
    added client info

--- init.sql
+++ init.sql
@@ -11,6 +11,30 @@ CREATE TABLE client (clientID int, name varchar(50), phoneNumber int, cellPhoneN
 CREATE TABLE trader (traderID int, name varchar(50), password varchar(50),
   primary key (traderID));
 
+INSERT INTO client (clientID, name, phoneNumber, cellPhoneNumber, email, isGold, stateName, zipcode, city, 
+stAddress, monthlyTrades, bitcoin, fiat, password)
+VALUES 
+(1, 'Morris Idale', 'midale0@hexun.com', 2027431201, 9707226069, '00 La Follette Drive', 
+'Washington',	'District of Columbia',	20010, 'Gold'),
+(2, 'Kimberly Liversidge', 'kliversidge1@histats.com', 5108140045, 2039456720, '29174 Eagle Crest Court',
+'Berkeley',	'California',	94712, 'Silver'),
+(3, 'Elisabet	Toplin', 'etoplin2@icq.com', 2057968196, 5205770850, '38702 Shopko Alley', 'Birmingham',
+'Alabama', 35210,	'Gold'),
+(4, 'Odilia	Glanz', 'oglanz3@bravesites.com', 4084186658, 3091972391, '6 Mitchell Street', 'San Jose',
+'California', 95123, 'Silver'),
+(5, 'Geordie Ianitti', 'gianitti4@studiopress.com', 2052921088, 2536156789, '7856 Rieder Court',
+'Birmingham',	'Alabama', 35231,	'Gold'),
+(6, 'Correy	Capun', 'ccapun5@sina.com.cn', 2059030758, 5592918109, '80269 Carey Lane', 'Birmingham',
+'Alabama', 35254,	'Gold'),
+(7, 'Wileen	Walkden',	'wwalkden6@howstuffworks.com', 7706556627, 5042783788, '31 Del Sol Court',
+'Lawrenceville', 'Georgia',	30245, 'Gold'),
+(8, 'Leontyne	Dullard',	'ldullard7@w3.org', 5123783612, 3209293110, '82 Pond Plaza', 'Austin',
+'Texas', 78789,	'Gold'),
+(9, 'Darbie	Sousa',	'dsousa8@nps.gov', 7135992952, 8162862891, '0 1st Center', 'Spring',
+'Texas', 77386,	'Silver'),
+(10, 'Davine Masic', 'dmasic9@studiopress.com', 3186085177,	3216507379,	'8 Maryland Center',
+'Shreveport', 'Louisiana', 71105, 'Gold');
+
 CREATE TABLE transactions (transactionID int, clientID int, traderID int, transactionDate date,
   primary key (transactionID),
   foreign key (clientID) references client(clientID),
